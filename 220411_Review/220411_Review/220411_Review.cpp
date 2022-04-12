be9559b18c66f7dc7483518f9905a61dfc99ac98
roomMaker <leehuk0622@gmail.com>
2022-04-12 09:20:02 +0900

    Update 220411_Review.cpp

--- 220411_Review/220411_Review/220411_Review.cpp
+++ 220411_Review/220411_Review/220411_Review.cpp
@@ -132,18 +132,37 @@ using namespace std;
 
 int main()
 {
+    //srand(time(NULL));
+    //int minNumber, maxNumber, randNumber, userNumber;
+    //randNumber = rand() % 8;
+    //randNumber <= 2 ? randNumber += 3 : randNumber;
+    //cout << randNumber << endl;//1번
+
+    //cout << "최대값을 입력 : ";
+    //cin >> maxNumber;
+    //cout << "최솟값을 입력 : ";
+    //cin >> minNumber;
+    //randNumber = (rand() % maxNumber) + 1;
+    //randNumber <= (minNumber - 1) ? randNumber += minNumber : randNumber;
+    //cout << randNumber << endl;//2번
+
+    //randNumber = (rand() % 9) + 1;
+    //cout << "1 ~ 9 사이의 숫자 입력 : ";
+    //cin >> userNumber;
+    //cout << boolalpha << (randNumber == userNumber) << endl;//3번
+
     srand(time(NULL));
     int minNumber, maxNumber, randNumber, userNumber;
-    randNumber = rand() % 8;
-    randNumber <= 2 ? randNumber += 3 : randNumber;
-    cout << randNumber << endl;//1번
+    randNumber = rand() % 5 + 3;
+    //randNumber <= 2 ? randNumber += 3 : randNumber;
+    cout << randNumber << endl; //1번
 
     cout << "최대값을 입력 : ";
     cin >> maxNumber;
     cout << "최솟값을 입력 : ";
     cin >> minNumber;
-    randNumber = (rand() % maxNumber) + 1;
-    randNumber <= (minNumber - 1) ? randNumber += minNumber : randNumber;
+    randNumber = rand() % ((maxNumber - minNumber) + 1) + minNumber;
+    //randNumber <= (minNumber - 1) ? randNumber += minNumber : randNumber;
     cout << randNumber << endl;//2번
 
     randNumber = (rand() % 9) + 1;
