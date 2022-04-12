d5a2258051fd35cfd0189d6d121d4e27608e75d1
roomMaker <leehuk0622@gmail.com>
2022-04-12 17:41:29 +0900

    Update 220412_Control.cpp

--- 220412_Control/220412_Control/220412_Control.cpp
+++ 220412_Control/220412_Control/220412_Control.cpp
@@ -80,6 +80,28 @@ using namespace std;
         while(조건식);     // 실행문을 무조건 한번 실행한 뒤 while 조건식에 따라 반복할지 말지 결정 (조건식 뒤 세미콜론 유의)
 
         실습) 전에 만든 숫자 맞추기 프로그램을 while문을 이용하여 정답을 맞출 때까지 반복되는 프로그램으로 수정해보자.
+
+        2) for 문
+
+        for( (1)초기식 ; (2)조건식 ; (3)증감식 )
+        {
+            (4)실행문;
+        }
+
+        (1) -> (2) -> (4) -> (3) -> (2) -> (4) -> (3) -> (2) -> (4) -> (3) -> .... 조건식의 결과값이 0(false)가 나올 때까지
+
+        실습(for) : 
+        1. 10부터 1까지 차례대로 출력되는 코드를 작성해 보자.
+        2. 100미만의 3의 배수를 차례대로 출력하는 코드를 작성해 보자.
+        3. 1000미만의 2의 승수를 차례대로 출력하는 코드를 작성해보자.
+        4. 구구단을 출력해보자.
+            1) 2단 차례대로 출력
+            2) 2단부터 9단까지 출력
+            3) 구구단을 2  3  4
+                       5  6  7
+                       8  9
+               단 순으로 출력
+
 */
 
 
@@ -166,7 +188,7 @@ int main()
     } 
     while (count < 5);*/
 
-    srand(time(NULL));
+    /*srand(time(NULL));
     int randNumber, userNumber, countNumber = 0;
     randNumber = (rand() % 9) + 1;
 
@@ -178,13 +200,143 @@ int main()
             break;
         }
             cout << "다시 시도해 보세요" << endl;
-    }
+    }*/ //실습
 
-    userNumber = -1;
+    /*userNumber = -1;
     while (randNumber != userNumber) {
         cout << "1 ~ 9 사이의 숫자 맞추기!! : ";
         cin >> userNumber;
        
         (randNumber == userNumber) ? cout << "정답!" << endl : cout << "다시 시도해 보세요" << endl;
+    }*/ //실습
+
+    /*for (int i = 0; i < 5; i++) {
+        cout << "Hello World" << endl;
+    }*/
+    
+
+    
+    /*
+    cout << "10부터 1까지 출력 : ";
+    for (i = 10; i > 0; i--) {
+        cout <<  i << " ";
     }
+    cout << endl;
+    cout << "100미만 3의배수 : ";
+    for (i = 3; i < 100; i+=3) {
+        cout << i << " ";
+    }
+    cout << endl;
+    cout << "1000미만 2의 승수 : ";
+    for (i = 1; i < 1000; i*=2) {
+       cout << i << " ";
+    }
+    cout << endl;*/
+
+    /*int i;
+    
+    for (i = 1; i < 10; i++) {
+        cout << "2 * " << i << " = " << 2 * i << endl; 
+    }
+    cout << endl;
+
+    for (i = 2; i < 10; i++) {
+        for (int j = 1; j < 10; j++) {
+            cout << i << " * " << j << " = " << i * j << endl;
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+    for (int k = 2; k < 9; k += 3) {
+        for (i = 1; i < 10; i++) {
+            for (int j = k; j < k + 3; j++) {
+                if (j == 10) {
+                    break;
+                }
+                cout << j << " * " << i << " = " << i * j << "\t";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }*/
+
+    for (int i = 1; i < 6; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    } // 1번
+    cout << endl;
+
+    for (int i = 1; i < 6; i++) {
+        for (int j = 4; j >= i; j--) {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    } // 2번
+    cout << endl;
+
+    for (int i = 5; i > 0; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    } // 3번
+    cout << endl;
+
+    for (int i = 1; i < 6; i++) {
+        for (int j = 2; j <= i ; j++) {
+            cout << " ";
+        }
+        for (int j = 5; j >= i; j--) {
+            cout << "*";
+        }
+        cout << endl;
+    } // 4번
+    cout << endl;
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 4; j >= i; j--) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i+i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    } // 5번
+    cout << endl;
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << " ";
+        }
+        for (int j = 8; j >= i+i; j--) {
+            cout << "*";
+        }
+        cout << endl;
+    } // 6번
+    cout << endl;
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 4; j >= i; j--) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i+i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i < 5; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << " ";
+        }
+        for (int j = 8; j >= i + i; j--) {
+            cout << "*";
+        }
+        cout << endl;
+    }// 7번
 }
